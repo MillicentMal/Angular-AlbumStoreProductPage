2d3575bee79a6f221e7b9472f3e15eb6198ad0fb
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2021-12-12 23:58:36 +0200

    Final Push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,11 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import {RouterModule, Routes} from "@angular/router";
-appRoutes : Routes[]
+const appRoutes : Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
+] 
 @NgModule({
   declarations: [
     AppComponent,
