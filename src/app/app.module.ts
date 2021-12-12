886d3bf7f756148834c8c447990c94f961ce8081
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2021-12-12 23:48:19 +0200

    Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
-
+import {RouterModule, Routes} from "@angular/router";
+appRoutes : Routes[]
 @NgModule({
   declarations: [
     AppComponent,
